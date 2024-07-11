5154b7505462365b2913d19c9455821fc8887a52
GustavoCesias <cesiasgustavo@gmail.com>
2024-07-11 14:39:49 -0500

    Creación del proyecto

---
+++ src/main/java/org/cesias/gimnasiocertus/GimnasioCertusApplication.java
@@ -0,0 +1,13 @@
+package org.cesias.gimnasiocertus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimnasioCertusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GimnasioCertusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cesias/gimnasiocertus/GimnasioCertusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cesias.gimnasiocertus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasioCertusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
